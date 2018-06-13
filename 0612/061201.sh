60cfb86cba01d87db4af4e6e3ba82ee2507459c1
Oomija Taku <n18005@std.it-college.ac.jp>
2018-06-13 07:00:25 +0000

    修正

--- 0612/061201.sh
+++ 0612/061201.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-cd /home/vagrant
- file= du -h /home/vagrant/* |sort -nr | head -n5 |awk '{print $2, $1}'
-    
-
-
-
-
+find ~ -type f | sed 's/\(.*\)/\"\1\"/' | xargs du -b | sort -nr | head -n5 | awk '{print $2}' | xargs du -h
